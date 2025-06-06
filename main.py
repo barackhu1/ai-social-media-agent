a437691fb1a3f759262ab3622a326cc20bc275f7
Adrián Negrea <barackhu1@gmail.com>
2025-06-07 00:30:21 +0200

    loading API key

---
+++ main.py
@@ -0,0 +1,10 @@
+# importing built-in library
+import os
+import json
+import requests
+# importing installed library
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
