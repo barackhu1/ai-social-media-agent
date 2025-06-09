7853fc9b8042ea8a403dac5fc6d987ab192fb459
Adrián Negrea <barackhu1@gmail.com>
2025-06-09 21:54:33 +0200

    prompt added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # importing built-in library
 import os
 import json
-import requests
 # importing installed library
+import requests
 from dotenv import load_dotenv
+from jinja2 import Template
+
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
@@ -11,7 +13,7 @@ API_KEY = os.getenv("API_KEY")
 def get_content(response):
     return response.json().get("choices", {})[0].get("message", {}).get("content")
 
-def get_response():
+def get_response(prompt):
     return requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
@@ -23,12 +25,16 @@ def get_response():
         "messages": [
             {
                 "role": "user",
-                "content": "What is the meaning of life?"
+                "content": prompt
             }
         ],
     })
 )
 
 if __name__ == '__main__':
-    response = get_response()
+    use_emoji = True
+    emoji_text = "use emoji" if use_emoji else "don't use emoji"
+    template = Template("Write a {{ tone }} post to {{ audience }} target audience, {{ emoji }} ,with the next campaign message: '{{ message }}', and the response should be in json like this: {'facebook': '<text + recomended hashtags>', 'instagram': '<tags + hashtags + 2 recomended picture ideas>','linkedin': '<text>', 'x': '<text + hashtags>'}")
+    prompt = template.render(tone="friendly", audience="20-30 age gamer", emoji=emoji_text, message="Find out our new chair!")
+    response = get_response(prompt)
     print(get_content(response))
