cd2356105a78c254b1f61a3b90c2c90420a18953
Adrián Negrea <barackhu1@gmail.com>
2025-06-09 21:05:49 +0200

    Calling Deepseek AI

--- main.py
+++ main.py
@@ -8,3 +8,27 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+def get_content(response):
+    return response.json().get("choices", {})[0].get("message", {}).get("content")
+
+def get_response():
+    return requests.post(
+    url="https://openrouter.ai/api/v1/chat/completions",
+    headers={
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+    },
+    data=json.dumps({
+        "model": "deepseek/deepseek-r1-0528-qwen3-8b:free",
+        "messages": [
+            {
+                "role": "user",
+                "content": "What is the meaning of life?"
+            }
+        ],
+    })
+)
+
+if __name__ == '__main__':
+    response = get_response()
+    print(get_content(response))
