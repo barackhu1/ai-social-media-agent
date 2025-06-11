fe9b9e80a14e12b31ec2d06c5ac60e96d6b8e1df
Adrián Negrea <barackhu1@gmail.com>
2025-06-11 21:30:45 +0200

    documenting modules.py

--- modules.py
+++ modules.py
@@ -1,21 +1,13 @@
 # importing built-in library
 import json
+from typing import Optional
 # importing installed library
 import requests
 # importing other packages
 from exceptions import RateLimitExceededError, NoModelProvidedError
 
-def get_content(response):
-    if response.status_code == 200:
-        return response.json().get("choices", {})[0].get("message", {}).get("content")
-    elif response.status_code == 400:
-        raise NoModelProvidedError("No model provided, please provide one in .env file.")
-    elif response.status_code == 429:
-        raise RateLimitExceededError("Daily rate limit exceeded, limit is 50, you have 0 remaining. Try again tomorrow.")
-    else:
-        print(f"Error: {response.status_code}, Message: {response.text}")
-
-def get_response(prompt, api_key, model):
+# getting response with API call
+def get_response(prompt: str, api_key: str, model: str) -> str:
     return requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
@@ -33,7 +25,23 @@ def get_response(prompt, api_key, model):
     })
 )
 
-def data_clean(raw):
+# returning the output from the response
+def get_content(response: str) -> Optional[str]:
+    # in case of status OK the output string will be returned
+    if response.status_code == 200:
+        return response.json().get("choices", {})[0].get("message", {}).get("content")
+    # model was no provided raising error
+    elif response.status_code == 400:
+        raise NoModelProvidedError("No model provided, please provide one in .env file.")
+    # if the limit of the requests are out raising error
+    elif response.status_code == 429:
+        raise RateLimitExceededError("Daily rate limit exceeded.")
+    # raising error on other status codes
+    else:
+        print(f"Error: {response.status_code}, Message: {response.text}")
+
+# cleaning output from unwanted characters, not always accruate, depends on the output format
+def data_clean(raw: str) -> str:
     if isinstance(raw, str):
         cleared_string = raw.replace("json\n", "")
         cleared_string = cleared_string.replace("`","")
